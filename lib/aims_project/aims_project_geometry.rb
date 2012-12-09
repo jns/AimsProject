bbb1c8757f2074365a6d31bc29c63bff7761a1f7
Joshua Shapiro <joshua.shapiro@gmail.com>
2012-12-09 10:22:38 -0800

    Added AimsProjectGeometry This class encapsulated (and delegates?) to Aims::Geoemtry but also includes additional state such as the original ruby code used to define the geometry, and cached versions of the corrected geoemetries, and maybe efficient algorithms for computing and calculating repeated geometries

---
+++ lib/aims_project/aims_project_geometry.rb
@@ -0,0 +1,46 @@
+module AimsProject
+  
+  # A class that encapsulates the Aims::Geometry and other
+  # state information important for AimsProject
+  class AimsProjectGeometry
+    
+    # Boolean flag indicating whether the atoms should be displaced to all lie
+    # within the unit cell
+    attr_accessor :correct_geometry
+
+    # The filename defining this geometry
+    attr_reader :filename
+    
+    def initialize(file)
+      @filename = file
+      @geometry_original = GeometryParser.parse(@filename)
+    end
+    
+    def atoms
+      if self.correct_geometry
+        geometry_corrected
+      else
+        geometry_original
+      end
+    end
+    
+    def geometry_corrected
+      if @geometry_corrected.nil?
+        @geometry_corrected = @geometry_original.correct
+      end
+      @geometry_corrected
+    end
+    
+    def geometry_original
+      @geometry_original
+    end
+    
+    def save
+      unless @filename
+        raise InvalidFilenameException
+      end
+    end
+    
+  end
+  
+end
