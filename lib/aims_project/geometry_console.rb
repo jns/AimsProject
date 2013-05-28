77c61902fed29dfbec1b720ddddfad07d9bd7e01
Joshua Shapiro <joshua.shapiro@gmail.com>
2013-05-28 12:56:37 -0700

    Beginning of console implementation

--- lib/aims_project/geometry_console.rb
+++ lib/aims_project/geometry_console.rb
@@ -14,48 +14,73 @@ module AimsProject
       set_sizer(sizer)
       prompt
       
-      def @text_ctrl.on_char(evt)
-        k = evt.key_code
-        mflag = evt.modifiers
-
+      @text_ctrl.evt_key_down do |evt|
+        k = evt.get_key_code
         case k
-        when K_RETURN
-          if evt.inputmod_down
-            # multi-line command uses meta-down-arrow for newline
-            self.write_text("\n")
-          else
-            @history << self.value
-            run self.value
-            self.clear
+        when K_UP
+          echo("Key Up")
+        when K_DOWN
+          echo("Key Down")
+        when K_LEFT
+          caret_pos = @text_ctrl.get_insertion_point
+          if caret_pos > @line_start
+            @text_ctrl.set_insertion_point(caret_pos-1)
           end
-          return
-        when (evt.inputmod_down and K_UP)
-          if hist=history.prev
-            self.value = hist
-            self.set_insertion_point_end
-            return
-          end
-        when (evt.inputmod_down and K_DOWN)
-          if hist=history.next
-            self.value = hist
-            self.set_insertion_point_end
-            return
-          else
-            self.clear
-          end
+        when K_RIGHT
+          caret_pos = @text_ctrl.get_insertion_point
+          if caret_pos < @text_ctrl.get_last_position
+            @text_ctrl.set_insertion_point(caret_pos+1)
+          end          
+        else
+          evt.skip
         end
-        evt.skip()
       end
       
+      @text_ctrl.evt_char do |evt|
+        k = evt.get_key_code
+        case k
+        when K_RETURN
+          cmd = @text_ctrl.get_range(@line_start, @text_ctrl.get_caret_position+1).strip
+          print("\n")
+          
+          # evaluate
+          begin
+            result = get_binding.eval(cmd)
+            print(result.to_s + "\n")
+          rescue 
+            print $!.to_s
+            print "\n"
+          end
+          
+          prompt
+        when K_HOME # For some reason, ctrl-a maps to this
+          @text_ctrl.set_insertion_point(@line_start)
+        when K_HELP # For some reason, ctrl-e maps to this
+          @text_ctrl.set_insertion_point_end
+        else
+          evt.skip()
+        end
+      end
+      
+    end
+    
+    def get_binding
+      unless @binding
+        @binding = binding()
+      end
+      @binding
     end
     
     def prompt
       @text_ctrl.append_text(">> ")
       @text_ctrl.move_end
-      @insertion_point = @text_ctrl.get_caret_position
-      @text_ctrl.show_position(@insertion_point)
+      @line_start = @text_ctrl.get_caret_position
+      @text_ctrl.show_position(@line_start)
     end
     
+    def print(str)
+      @text_ctrl.append_text(str)
+    end
     
     def echo(str)
       @text_ctrl.append_text("\n" + str + "\n")
